b6149be0d1233024e1d0e899d2cc93aa881c2fb3
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-11-18 12:27:32 +0600

    user information load

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wyy6auz.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -26,8 +26,20 @@ async function run() {
 
 
     const menuCollection = client.db("BistroBossDB").collection("menu");
+    const usersCollection = client.db("BistroBossDB").collection("users");
     const cartCollection = client.db("BistroBossDB").collection("carts");
 
+    // user related api
+    app.post("/users", async(req, res)=>{
+      const users = req.body
+      const query = {email: users.email}
+      const existingUser = await usersCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: "user already exist", insertedId: null})
+      }
+      const result = await usersCollection.insertOne(users)
+      res.send(result)
+    })
 
 
     app.get("/menu", async (req, res) => {
@@ -44,6 +56,13 @@ async function run() {
       const email = req.query.email;
       const query = {email: email}
       const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.delete('/cart/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
