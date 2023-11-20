46f6ec7ff2d100270b8e2f0b9f0b97026d1b8901
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-11-21 02:46:47 +0600

    verious api created

--- index.js
+++ index.js
@@ -31,10 +31,54 @@ async function run() {
     const usersCollection = client.db("BistroBossDB").collection("users");
     const cartCollection = client.db("BistroBossDB").collection("carts");
     // jwt related api
+    app.post('/jwt', async(req, res)=>{
+      const userEmail = req.body;
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token})
+    })
 
+    const verifyToken = (req, res, next)=>{
+        if(!req?.headers?.authorization){
+          return res.status(401).send({message: "Unauthorized Access"})
+        }
+        const token = req.headers.authorization.split(' ')[1]
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+          if(error){
+            return res.status(401).send({message: "Unauthorized Access"})
+          }
+          res.decoded = decoded
+          next()
+        })
+      }
 
+      // verify admin 
+      const verifyAdmin = async(req, res, next)=>{
+        const email = res.decoded.email
+        const query = {email: email}
+        const user = await usersCollection.findOne(query)
+        isAdmin = user?.role === "admin"
+        if(!isAdmin){
+          return res.status(403).send({message: "Forbidden access"})
+        }
+        next()
+      }
+
+    // admin check 
+    app.get("/users/admin/:email",async(req, res)=>{
+      const email = req.params.email
+      // if(email !== res?.decoded?.email){
+      //   return res.status(403).send({message: "Forbidden access"})
+      // }
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      let admin = false
+      if(user){
+        admin = user?.role === "admin"
+      }
+      res.send({admin})
+    })
     // user related api
-    app.post("/users", async(req, res)=>{
+    app.post("/users",verifyToken, async(req, res)=>{
       const users = req.body
       const query = {email: users.email}
       const existingUser = await usersCollection.findOne(query)
@@ -46,19 +90,20 @@ async function run() {
     })
 
 
-    app.get("/users", async (req, res) => {
+    app.get("/users",verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
 
-    app.delete('/users/:id', async(req, res)=>{
+
+    app.delete('/users/:id',  verifyToken,verifyAdmin, async(req, res)=>{
       const id = req.params.id
       const query = {_id: new ObjectId(id)}
       const result = await usersCollection.deleteOne(query)
       res.send(result)
     })
 
-    app.patch('/users/admin/:id', async(req, res)=>{
+    app.patch('/users/admin/:id',  verifyToken, async(req, res)=>{
       const id = req.params.id
       const query = {_id: new ObjectId(id)}
       const options = { upsert: true };
@@ -77,6 +122,12 @@ async function run() {
     // menu related api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post("/menu", async (req, res) => {
+      const data = req.body
+      const result = await menuCollection.insertOne(data)
       res.send(result)
     })
     
